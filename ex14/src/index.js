ffc7b3318c2a1f4ab70bd939aeb87740c8746376
AnelB <anel.b@live.com>
2022-04-07 20:59:55 +0200

    pet up

---
+++ ex14/src/index.js
@@ -0,0 +1,17 @@
+var myArr = [2, 5, 9, 8, 1];
+
+function bubblesort(arr){
+   for (var i = 0; i < arr.length; i++){
+       for (var j = 1 ; j < (arr.length-i); j++){
+           if(arr[j-1] > arr[j]){
+            temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+           }
+     
+       }
+    }
+    return arr;
+   } 
+console.log(bubblesort(myArr));
+module.exports = bubblesort;
