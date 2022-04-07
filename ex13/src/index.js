2cd5af2d6c3ec5188031d9f4e42c2fbc3baec137
AnelB <anel.b@live.com>
2022-04-07 19:18:28 +0200

    treci up

---
+++ ex13/src/index.js
@@ -0,0 +1,12 @@
+var str ="Paragon!";
+
+function myReverse(str){
+    var result = "";
+
+    for (var i = str.length - 1; i >= 0; i -= 1)
+    {
+        result += str[i];
+    }
+    return result;
+}
+console.log(myReverse(str));
