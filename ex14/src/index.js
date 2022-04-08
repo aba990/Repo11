46c9281cb6a5bc8f4eb92d8429611f57aea5484f
AnelB <anel.b@live.com>
2022-04-08 06:54:11 +0200

    zadnji up

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -1,6 +1,6 @@
 var myArr = [2, 5, 9, 8, 1];
 
-function bubblesort(arr){
+function bubbleSort(arr){
    for (var i = 0; i < arr.length; i++){
        for (var j = 1 ; j < (arr.length-i); j++){
            if(arr[j-1] > arr[j]){
@@ -14,4 +14,4 @@ function bubblesort(arr){
     return arr;
    } 
 console.log(bubblesort(myArr));
-module.exports = bubblesort;
+module.exports = bubbleSort;
