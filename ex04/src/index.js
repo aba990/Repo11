6fe7a440a2783bbba1bffc991d028971e55017f8
AnelB <anel.b@live.com>
2022-04-09 17:27:06 +0200

    ispravka

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,15 +1,25 @@
-averageDayTemp = [];
 
 var temps = [
- monday = [31, 32, 19, 37],
-tuesday = [29, 27, 55, 36],
-wednesday = [17, 27, 42, 46],
-thursday = [29, 52, 21, 64],
-friday = [91, 27, 31, 61]   
+[31, 32, 19, 37],
+ [29, 27, 55, 36],
+[17, 27, 42, 46],
+[29, 52, 21, 64],
+[91, 27, 31, 61]   
 ]
+temps[0][2]
 
-function myArrayFunction(obj) {
-    for (var i = 0; i < obj.length; i++) {
-        let temp = 0.0;
-        for (var j = 0; j < obj1[i].length; j++) {
-            temp = temp + o
+function myArrayFunction(celsius) {
+    var averageDayTemp = [];
+    var sum = 0;
+
+    for (var i = 0; i < celsius.length; i++) {
+        sum = 0;
+    for (var j = 0; j < celsius[i].length; j++) {
+        sum += celsius[i][j];
+    }
+    averageDayTemp.push(sum / 4);
+    }
+    return averageDayTemp;
+}
+console.log(myArrayFunction(temps));
+module.exports = myArrayFunction;
